7e7681ff510537611da6f5b17e5ed1a26aba3a5f
gyeonggumun <lion989072@gmail.com>
2025-12-19 14:15:28 +0900

    posts formdata형식으로 받아오기

---
+++ src/main/java/com/korit/post_mini_project_back/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.korit.post_mini_project_back.controller;
+
+import com.korit.post_mini_project_back.dto.request.CreatePostReqDto;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    // 기본으로는 데이터를 Json으로 받기 때문에 FormData로 받도록 type 세팅
+    // value는 위에서 주소를 설정해주었기 때문에 따로 설정해주지 않아도 괜찮다
+    // formData형식으로 데이터를 받을 때는 @ModelAttribute 언어테이션을 달아줌 (dto사용한정)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> createPost(@ModelAttribute CreatePostReqDto dto) {
+        System.out.println(dto);
+        return ResponseEntity.ok(null);
+    }
+}
